a67b5d3146276841b154d1f941ca26b81c39a5a5
Brandon Sanchez <bsanch05@nyit.edu>
2025-11-25 12:26:20 -0500

    added function to add new items to list

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 
 class NewItem extends StatefulWidget{
   const NewItem({super.key});
@@ -11,11 +12,73 @@ class _NewItemState extends State<NewItem>{
   Widget build(BuildContext context)
   {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Add a new item...")
-      ),
-      body: Padding(padding: EdgeInsets.all(12),
-      child: Text("Form Goes here")
+      appBar: AppBar(title: Text("Add a new item...")),
+      body: Padding(
+        padding: EdgeInsets.all(12),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+              maxLength:50,
+              decoration: InputDecoration(label: Text('Name')),
+              validator: (value){
+                if(value==null || value.isEmpty || value.length <=1 || value.length > 50)
+                {
+                  return "Must have a name between 2 and 50 characters long!";
+                }
+                return null;
+              },
+            ),
+          
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Expanded( 
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    label:Text("Quantity"),
+                  ),
+                  initialValue: '1',
+                  validator: (value){
+                    if (value == null ||
+                        value.isEmpty ||
+                        int.tryParse(value)==null ||
+                        int.tryParse(value)! <=0){
+                          return "Must Enter a quantity greater than 0";
+                        }
+                        return null;
+                  }
+                ),
+                ),
+                const SizedBox(width:8),
+                Expanded(
+                  child: DropdownButtonFormField(items: [
+                    for(final category in categories.entries)
+                      DropdownMenuItem(
+                        value:category.value,
+                        child: Row(children: [
+                        Container(
+                          width:16,
+                          height:16,
+                          color:category.value.color
+                        ),
+                        const SizedBox(width:6),
+                        Text(category.value.title),
+                      ],)
+                      ,)
+                  ], onChanged: (value){}),
+                )
+              ],
+              ),
+            const SizedBox(height:12,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+              TextButton(onPressed: (){}, child: const Text("Reset")),
+              ElevatedButton(onPressed: (){}, child: Text("Add Item"))
+            ],)
+        ],) 
+      )
       ),
     );
   }
