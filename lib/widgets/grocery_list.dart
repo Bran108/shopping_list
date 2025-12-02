7431e0a62aaf71d2991370f243167f872e9a3271
Brandon Sanchez <bsanch05@nyit.edu>
2025-12-02 12:33:50 -0500

    finished add items

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -24,17 +24,28 @@ class _GroceryListState extends State<GroceryList> {
         });
       }
 }
+void _removeItems(GroceryItem item){
+  setState((){
+    _groceryItems.remove(item);
+  });
+  for (var i = 0; i <_groceryItems.length; i++){
+    print(_groceryItems[i].name);
+  }
+}
 
 @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Your Groceries"),
-        actions: [IconButton(onPressed: _addItem, icon: Icon(Icons.add_box))],
-      ),
-      body:ListView.builder(
+    Widget content = const Center(child:Text("Please Click the + Button to add an Item"));
+if(_groceryItems.isNotEmpty)
+{
+      content: ListView.builder(
         itemCount: _groceryItems.length,
-        itemBuilder: (ctx, index) => ListTile(
+        itemBuilder: (ctx, index) => Dismissible(
+          onDismissed: (direction){
+            _removeItems(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
           leading: Container(
             width: 24,
             height: 24,
@@ -43,7 +54,15 @@ class _GroceryListState extends State<GroceryList> {
           title: Text(_groceryItems[index].name),
           trailing: Text(_groceryItems[index].quantity.toString()),
         ),
-      )
+      ),
+    );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your Groceries"),
+        actions: [IconButton(onPressed: _addItem, icon: Icon(Icons.add_box))],
+      ),
+      body: content,
     );
   }
 }
